d7e25f927c36b8c0ab4a7da968b8bb551fd03e73
WellingtonZanelatto <110206532+WellingtonZanelatto@users.noreply.github.com>
2023-10-19 21:50:54 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import unittest
+import numeroromano as nr
+
+class TestConverteRomano(unittest.TestCase):
+
+    def test_I(self):
+        self.assertEqual(nr.converte('I'), 1)
+
+    def test_V(self):
+        self.assertEqual(nr.converte('V'), 5)
+
+    def test_II(self):
+        self.assertEqual(nr.converte('II'), 2)
+
+    def test_XXII(self):
+        self.assertEqual(nr.converte('XXII'), 22)
+
+    def test_IX(self):
+        self.assertEqual(nr.converte('IX'), 9)
+
+    def test_XXIV(self):
+        self.assertEqual(nr.converte('XXIV'), 24)
+
+
+if __name__ == '__main__':
+    unittest.main()
